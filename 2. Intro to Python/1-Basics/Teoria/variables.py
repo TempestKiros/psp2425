1c1aab99e5e40639cde4cef44ff2a4b4c9727b18
Deep Feeder <gamma.deep.feeders@gmail.com>
2024-10-26 15:19:18 +0200

    "Updated execution counts and added output values in examen_basics.ipynb"

---
+++ 2. Intro to Python/1-Basics/Teoria/variables.py
@@ -0,0 +1,13 @@
+first_name = 'John'
+last_name = 'Mc Arthur'
+full_name = 'John Mc arthur' 
+country = "España"
+city = "Madrid"
+age = 30
+year = 2024
+is_married = False
+is_true = True
+is_light_on = False
+
+# Declaración de múltiples variables en una sola línea
+x, y, z = 5, 10, 15
